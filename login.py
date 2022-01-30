0983ab54d35f03a66d98ed8249041e919bf2dbc1
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-01-30 01:16:37 +0100

    Add files via upload

---
+++ login.py
@@ -0,0 +1,23 @@
+from http.client import HTTPS_PORT, HTTPConnection
+from math import pi
+from nturl2path import url2pathname
+from tokenize import String
+import requests
+import errors
+
+def login( email, password):
+
+    print('/ FEW CLICKS MORE//L' )
+post_url='https://facebook.com/login.php'
+headers {
+}
+print('/ /please install mechanize //')
+sys.exit
+
+print('/// Your Facebook Random Pentest ////')
+password=str(raw_input('name of your password.txt:  ').strip())
+email=str(raw_input('open your email list +"filename.txt":   ').string())
+
+
+
+
