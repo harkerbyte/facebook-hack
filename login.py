d6a5b22f5ee6d50664deb54c8c0a0e3e8b22e878
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-02-08 00:10:42 +0100

    Add files via upload

--- login.py
+++ login.py
@@ -2,22 +2,21 @@ from http.client import HTTPS_PORT, HTTPConnection
 from math import pi
 from nturl2path import url2pathname
 from tokenize import String
-import requests
 import errors
 
 def login( email, password):
 
     print('/ FEW CLICKS MORE//L' )
-post_url='https://facebook.com/login.php'
-headers {
-}
-print('/ /please install mechanize //')
-sys.exit
+post_url=str(raw_input('Target web link:"   ').strip())
+header = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',}
 
 print('/// Your Facebook Random Pentest ////')
 password=str(raw_input('name of your password.txt:  ').strip())
-email=str(raw_input('open your email list +"filename.txt":   ').string())
-
-
-
-
+email=str(raw_input('input email of target: ').strip ())
+print("trying passwords with target email: " ,email)
+i=0
+while file :
+    password=file.readline().strip()
+    i+=1
+    if len(password) < 10:
+    
