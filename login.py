02d941e89534ce29231b40ab25b2bb427919cfa9
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-02-16 09:38:32 +0100

    Add files via upload

--- login.py
+++ login.py
@@ -1,22 +1,25 @@
+from argparse import FileType
+from ast import Continue
+import fileinput
 from http.client import HTTPS_PORT, HTTPConnection
 from math import pi
 from nturl2path import url2pathname
 from tokenize import String
-import errors
+
 
 def login( email, password):
 
     print('/ FEW CLICKS MORE//L' )
-post_url=str(raw_input('Target web link:"   ').strip())
+post_url=str("raw_input"('Target web link:"   ').strip())
 header = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',}
 
 print('/// Your Facebook Random Pentest ////')
-password=str(raw_input('name of your password.txt:  ').strip())
-email=str(raw_input('input email of target: ').strip ())
+password=str('raw_input'('name of your password.txt:  ').strip())
+email=str('raw_input'('input email of target: ').strip ())
 print("trying passwords with target email: " ,email)
 i=0
-while file :
-    password=file.readline().strip()
+while FileType :
+    password=fileinput.readline().strip()
     i+=1
-    if len(password) < 10:
-    
+if len(password) < 10:
+   Continue
